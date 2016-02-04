c79f6047c80ca654c01e9429e93063d03ebbb603
Abdul Hannan Ali <abdulhannanali@outlook.com>
2016-02-05 00:26:26 +0500

    add cat clicker premium with display area

--- js/main.js
+++ js/main.js
@@ -4,42 +4,75 @@ $(document).ready(function () {
 	// almost all the code was changed
 	var cats = [
 		{
-			name: "japanese cutie",
-			filename: "japancat.jpg"
-		},
-		{
 			name: "soooo cute",
 			filename: "cat1.jpg"
 		},
 		{
+			name: "japanese cutie",
+			filename: "japancat.jpg"
+		},
+		{
 			name: "inspiration",
 			filename: "cat2.jpg"
+		},
+		{
+			name: "Two cute cats",
+			filename: "twocutecats.jpg"
+		},
+		{
+			name: "Big cat and small cute cats",
+			filename: "zoocats.jpg"
+		},
+		{
+			name: "So cute it hurts",
+			filename: "hurtingcute.jpg"
 		}
 	]
 
+	var catsName = $("#catsName")
+	var catDisplay = $("#catDisplay")
+
 	cats.forEach(function (value, index, array) {
 		var clicks = 0
-		var title = $("<h1>").text(value.name).addClass("catTitle")
+		var sideTitle = $("<h1>").text(value.name).addClass("catTitle")
+		var displayTitle = $("<h1>").text(value.name)
 		var image = $("<img />")
 						.attr("src", "assets/" + value.filename)
 						.attr("alt", "cat pic " + index)
+						.addClass("catPic")
+
 		var timesClick = $("<h3>")
 		setTimesText(timesClick, clicks)
 
-		image.click(function (event) {
-			clicks++
-			setTimesText(timesClick, clicks)
-		})
 
-		function setTimesText(element, num) {
-			element.text("You have clicked this cute cat " + num + " " + (num > 1 ? "times" : "time"))
+		sideTitle.click(function (event) {
+			catsName.children().removeClass("clicked")
+			sideTitle.addClass("clicked")
+			displayCat()
+		}) 
+
+		catsName.append(sideTitle)
+
+		function setTimesText(element, clicks) {
+			element.text("You have clicked this cute cat " + clicks + " " + (clicks > 1 ? "times" : "time"))
 		}
 
-		var catDiv = $("<div class='catDiv'>")
-			.append(title)
-			.append(image)
-			.append(timesClick)
+		function displayCat() {
+			catDisplay.empty()
+			catDisplay.append(displayTitle)
+			catDisplay.append(timesClick)
+			catDisplay.append(image)
 
-		catsHolder.append(catDiv)
+			image.click(function (event) {
+				clicks++
+				setTimesText(timesClick, clicks)
+			})
+		}
+
+		if (index == 1) {
+			catsName.children()[0].click()
+		}
+
+
 	})
 })
