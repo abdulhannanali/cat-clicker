8b5b74797e952b954b6b42509cfe7258551421ac
Abdul Hannan Ali <abdulhannanali@outlook.com>
2016-02-04 22:37:11 +0500

    version 2 for displaying 2 cats using object and functional approach

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
-window.onload = function () {
-	var clickTimes = 0
-	var timesText = document.getElementById("timesText")
-	var catPic = document.getElementById("catPic")
+$(document).ready(function () {
+	var catsHolder = $("#catsHolder")
 
-	setTimesText(clickTimes)
-	catPic.addEventListener("click", function (event) {
-		clickTimes++
-		setTimesText(clickTimes)
+	// almost all the code was changed
+	var cats = [
+		{
+			name: "japanese cutie",
+			filename: "japancat.jpg"
+		},
+		{
+			name: "soooo cute",
+			filename: "cat1.jpg"
+		},
+		{
+			name: "inspiration",
+			filename: "cat2.jpg"
+		}
+	]
+
+	cats.forEach(function (value, index, array) {
+		var clicks = 0
+		var title = $("<h1>").text(value.name).addClass("catTitle")
+		var image = $("<img />")
+						.attr("src", "assets/" + value.filename)
+						.attr("alt", "cat pic " + index)
+		var timesClick = $("<h3>")
+		setTimesText(timesClick, clicks)
+
+		image.click(function (event) {
+			clicks++
+			setTimesText(timesClick, clicks)
+		})
+
+		function setTimesText(element, num) {
+			element.text("You have clicked this cute cat " + num + " " + (num > 1 ? "times" : "time"))
+		}
+
+		var catDiv = $("<div class='catDiv'>")
+			.append(title)
+			.append(image)
+			.append(timesClick)
+
+		catsHolder.append(catDiv)
 	})
-
-	function setTimesText(num) {
-		timesText.innerHTML = "You have clicked this cat " + num + " " + (num > 1 ? "times" : "time")
-	}
-}
+})
